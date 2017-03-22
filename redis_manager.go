902004d181a3efa52e90ac936166f569371b21d8
Ming <642203604@qq.com>
2017-03-22 19:50:13 +0800

    实现连接池的配置

--- redis_manager.go
+++ redis_manager.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	log "github.com/Sirupsen/logrus"
 	"github.com/garyburd/redigo/redis"
 	"time"
 )
@@ -24,17 +25,55 @@ type RedisManager struct {
 	port        int
 	password    string
 	db          int
+	pool        *redis.Pool
 }
 
 func NewRedisManager(host string, port int, password string, db int) *RedisManager {
-	return &RedisManager{
-		maxIdle: RedisManager
+	redisMgr := &RedisManager{
+		maxIdle:     RedisManagerDefaultMaxIdle,
+		maxActive:   RedisManagerDefaultMaxActive,
+		idleTimeout: RedisManagerDefaultIdleTimeout,
+		host:        host,
+		port:        port,
+		password:    password,
+		db:          db,
+		pool:        nil,
+	}
+	redisMgr.pool = redisMgr.init()
+	return redisMgr
+}
+
+func NewRedisManagerWithPool(host string, port int, password string, db int, maxIdle int, maxActive int, idleTimeout time.Duration) *RedisManager {
+	redisMgr := &RedisManager{
+		maxIdle:     maxIdle,
+		maxActive:   maxActive,
+		idleTimeout: idleTimeout,
+		host:        host,
+		port:        port,
+		password:    password,
+		db:          db,
+	}
+	redisMgr.pool = redisMgr.init()
+	return redisMgr
+}
+
+func (redisMgr *RedisManager) init() *redis.Pool {
+	return &redis.Pool{
+		MaxIdle:     1,
+		MaxActive:   10,
+		IdleTimeout: 30 * time.Second,
+		Dial: func() (redis.Conn, error) {
+			c, err := redis.Dial("tcp", fmt.Sprintf("%s:%d", redisMgr.host, redisMgr.port))
+			if err != nil {
+				return nil, err
+			}
+			c.Do("SELECT", string(redisMgr.db))
+			return c, nil
+		},
 	}
 }
 
-func (redisMgr *RedisManager) getClient() {}
-
-func (redisMgr *RedisManager) Set(key string, value string) error {
+func (redisMgr *RedisManager) Set(key string, str string) error {
 	return nil
 }
 
@@ -42,24 +81,14 @@ func (redisMgr *RedisManager) Get(key string) (string, error) {
 	return "", nil
 }
 
+func (redisMgr *RedisManager) SetObject(key string, obj interface{}) {}
+
+func (redisMgr *RedisManager) GetObject(key string, obj interface{}) {}
+
 func main() {
-	redisClient = &redis.Pool{
-		MaxIdle:     1,
-		MaxActive:   10,
-		IdleTimeout: 30 * time.Second,
-		Dial: func() (redis.Conn, error) {
-			c, err := redis.Dial("tcp", "127.0.0.1")
-			if err != nil {
-				return nil, err
-			}
-			c.Do("SELECT", "0")
-		}
-	}
-	c, err := redis.Dial("tcp", "127.0.0.1:6379")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	redisMgr := NewRedisManagerWithPool("127.0.0.1", 6379, "", 0, 1, 10, 30*time.Second)
+	log.Debug(redisMgr)
+	c := redisMgr.pool.Get()
 	defer c.Close()
 
 	c.Do("MULTI")

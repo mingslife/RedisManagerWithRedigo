44bb3c6378e37837553878c010ea2426ee606334
Ming <642203604@qq.com>
2017-03-23 11:43:44 +0800

    实现RedisManager的GetObject、SetObject和DelObject方法

--- redis_manager.go
+++ redis_manager.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	log "github.com/Sirupsen/logrus"
 	"github.com/garyburd/redigo/redis"
@@ -16,6 +18,13 @@ const (
 	RedisManagerDefaultPassword    = ""          // No use yet
 	RedisManagerDefaultDb          = 0           // No use yet
 	RedisManagerDefaultExpireTime  = 21600       // 6 hours
+)
+
+const (
+	RedisManagerStatusUncheck = iota
+	RedisManagerStatusChecked
+	RedisManagerStatusDirty
+	RedisManagerStatusError
 )
 
 type RedisManager struct {
@@ -80,6 +89,10 @@ func (redisMgr *RedisManager) getConnection() redis.Conn {
 	return redisMgr.pool.Get()
 }
 
+func (redisMgr *RedisManager) getStatusKey(key string) string {
+	return key + "/status"
+}
+
 func (redisMgr *RedisManager) Set(key string, str string) error {
 	c := redisMgr.getConnection()
 	defer c.Close()
@@ -111,14 +124,107 @@ func (redisMgr *RedisManager) Del(key string) error {
 	_, err := c.Do("DEL", key)
 	if err != nil {
 		log.Error(err.Error())
+	}
+	return err
+}
+
+func (redisMgr *RedisManager) SetObject(key string, obj interface{}) error {
+	c := redisMgr.getConnection()
+	defer c.Close()
+
+	bytes, e := json.Marshal(obj)
+	if e != nil {
+		log.Error(e.Error())
+		return e
+	}
+	statusKey := redisMgr.getStatusKey(key)
+	status := RedisManagerStatusUncheck
+
+	ok, err := redis.Bool(c.Do("EXISTS", statusKey))
+	if err != nil {
+		log.Error(err.Error())
 		return err
 	}
+	if ok {
+		v, err := redis.Int(c.Do("GET", statusKey))
+		if err != nil {
+			log.Error(err.Error())
+			return err
+		}
+		if v != RedisManagerStatusChecked {
+			status = RedisManagerStatusDirty
+		}
+	}
+	c.Do("MULTI")
+	c.Do("SET", key, bytes)
+	c.Do("SET", statusKey, status)
+	_, err = c.Do("EXEC")
+	if err != nil {
+		log.Error(err.Error())
+	}
+	return err
+}
+
+func (redisMgr *RedisManager) GetObject(key string, obj interface{}) (int, error) {
+	c := redisMgr.getConnection()
+	defer c.Close()
+
+	statusKey := redisMgr.getStatusKey(key)
+
+	status := RedisManagerStatusError
+	ok, err := redis.Bool(c.Do("EXISTS", statusKey))
+	if ok {
+		status, err = redis.Int(c.Do("GET", statusKey))
+		if err != nil {
+			log.Error(err.Error())
+			obj = nil
+			return RedisManagerStatusError, err
+		}
+		bytes, err := redis.Bytes(c.Do("GET", key))
+		if err != nil {
+			log.Error(err.Error())
+			obj = nil
+			return RedisManagerStatusError, err
+		}
+		err = json.Unmarshal(bytes, obj)
+		if err != nil {
+			log.Error(err.Error())
+			obj = nil
+			return RedisManagerStatusError, err
+		}
+	} else {
+		err = errors.New("RedisManager: has not status")
+		log.Error(err.Error())
+		obj = nil
+	}
+	return status, err
+}
+
+func (redisMgr *RedisManager) DelObject(key string) error {
+	c := redisMgr.getConnection()
+	defer c.Close()
+
+	statusKey := redisMgr.getStatusKey(key)
+
+	c.Do("MULTI")
+	c.Do("DEL", key)
+	c.Do("DEL", statusKey)
+	_, err := c.Do("EXEC")
+	return err
+}
+
+// TODO
+func (redisMgr *RedisManager) CheckObject(key string) error {
+	c := redisMgr.getConnection()
+	defer c.Close()
+
 	return nil
 }
 
-func (redisMgr *RedisManager) SetObject(key string, obj interface{}) {}
-
-func (redisMgr *RedisManager) GetObject(key string, obj interface{}) {}
+type Student struct {
+	Id   int
+	Name string
+}
 
 func main() {
 	redisMgr := NewRedisManagerWithPool("127.0.0.1", 6379, "", 0, 1, 10, 30*time.Second)
@@ -126,4 +232,14 @@ func main() {
 	v, _ := redisMgr.Get("test")
 	log.Info(v)
 	redisMgr.Del("test")
+	student := &Student{
+		Id:   1,
+		Name: "Ming",
+	}
+	redisMgr.SetObject("student/1", student)
+	obj := &Student{}
+	status, _ := redisMgr.GetObject("student/1", obj)
+	log.Info(obj)
+	log.Info(status)
+	redisMgr.DelObject("student/1")
 }

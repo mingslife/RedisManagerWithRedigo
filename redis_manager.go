bdab0c97bb6a7654412cafe3af491ee65bc63992
Ming <642203604@qq.com>
2017-03-23 10:32:52 +0800

    实现RedisManager的Get、Set和Del方法

--- redis_manager.go
+++ redis_manager.go
@@ -11,10 +11,11 @@ const (
 	RedisManagerDefaultMaxIdle     = 1
 	RedisManagerDefaultMaxActive   = 10
 	RedisManagerDefaultIdleTimeout = 30 * time.Second
-	RedisManagerDefaultHost        = "127.0.0.1"
-	RedisManagerDefaultPort        = 6379
-	RedisManagerDefaultPassword    = ""
-	RedisManagerDefaultDb          = 0
+	RedisManagerDefaultHost        = "127.0.0.1" // No use yet
+	RedisManagerDefaultPort        = 6379        // No use yet
+	RedisManagerDefaultPassword    = ""          // No use yet
+	RedisManagerDefaultDb          = 0           // No use yet
+	RedisManagerDefaultExpireTime  = 21600       // 6 hours
 )
 
 type RedisManager struct {
@@ -26,6 +27,7 @@ type RedisManager struct {
 	password    string
 	db          int
 	pool        *redis.Pool
+	expireTime  int64
 }
 
 func NewRedisManager(host string, port int, password string, db int) *RedisManager {
@@ -38,6 +40,7 @@ func NewRedisManager(host string, port int, password string, db int) *RedisManag
 		password:    password,
 		db:          db,
 		pool:        nil,
+		expireTime:  RedisManagerDefaultExpireTime,
 	}
 	redisMgr.pool = redisMgr.init()
 	return redisMgr
@@ -73,12 +76,44 @@ func (redisMgr *RedisManager) init() *redis.Pool {
 	}
 }
 
+func (redisMgr *RedisManager) getConnection() redis.Conn {
+	return redisMgr.pool.Get()
+}
+
 func (redisMgr *RedisManager) Set(key string, str string) error {
+	c := redisMgr.getConnection()
+	defer c.Close()
+
+	_, err := c.Do("SET", key, str)
+	if err != nil {
+		log.Error(err.Error())
+		return err
+	}
 	return nil
 }
 
 func (redisMgr *RedisManager) Get(key string) (string, error) {
-	return "", nil
+	c := redisMgr.getConnection()
+	defer c.Close()
+
+	v, err := redis.String(c.Do("GET", key))
+	if err != nil {
+		log.Error(err.Error())
+		return "", err
+	}
+	return v, nil
+}
+
+func (redisMgr *RedisManager) Del(key string) error {
+	c := redisMgr.getConnection()
+	defer c.Close()
+
+	_, err := c.Do("DEL", key)
+	if err != nil {
+		log.Error(err.Error())
+		return err
+	}
+	return nil
 }
 
 func (redisMgr *RedisManager) SetObject(key string, obj interface{}) {}
@@ -87,16 +122,8 @@ func (redisMgr *RedisManager) GetObject(key string, obj interface{}) {}
 
 func main() {
 	redisMgr := NewRedisManagerWithPool("127.0.0.1", 6379, "", 0, 1, 10, 30*time.Second)
-	log.Debug(redisMgr)
-	c := redisMgr.pool.Get()
-	defer c.Close()
-
-	c.Do("MULTI")
-	c.Do("SET", "test", "ming")
-	v, err := c.Do("EXEC")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(v)
+	redisMgr.Set("test", "huangzeming")
+	v, _ := redisMgr.Get("test")
+	log.Info(v)
+	redisMgr.Del("test")
 }
